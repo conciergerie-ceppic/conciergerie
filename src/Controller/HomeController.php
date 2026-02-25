9afcd3a93be6364ee2e893a56933f5cc8fd08437
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-02-25 19:12:38 +0100

    Fix synthaxe Twig/Js + propriété float

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,11 +18,9 @@ final class HomeController extends AbstractController
         $session = $request->getSession();
         $lat = $session->get('lat');
         $lon = $session->get('lon');
-        $ville = null;
-        if (is_numeric($lat && $lon)) {
-            $ville = $this->getVille($hci,$lat,$lon);
-            $session->set('ville',$ville);
-        }
+        $ville = $this->getVille($hci,(float)$lat,(float)$lon);
+        $session->set('ville',$ville);
+        
         return $this->render('home/index.html.twig', [
             'services' => $services,
             'lat' => $lat,
