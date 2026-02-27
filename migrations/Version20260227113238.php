5e80c83910fb786ed2d1ce215c381b9f276b248c
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-02-27 12:39:19 +0100

    Suppression des versions

--- migrations/Version20260227113238.php
+++ migrations/Version20260227113238.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260227091935 extends AbstractMigration
+final class Version20260227113238 extends AbstractMigration
 {
     public function getDescription(): string
     {
