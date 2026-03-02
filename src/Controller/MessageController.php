e2866a53d25b405b37cd356a5a80abd2636d8def
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-03-02 15:25:55 +0100

    Gestion erreur du contact

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Message;
-use App\Entity\User;
-use App\Repository\MessageRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,33 +13,42 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message')]
-    public function index(Request $req, MessageRepository $MsgRepo, EntityManagerInterface $emi): Response
+    public function index(Request $req, EntityManagerInterface $emi): Response
     {
         $data = $req->request->all();
-         if (!$data ) {
-          $error = "Veuillez renseigner tout les champs";
-          return $this->render("home/index.html.twig", [
-            'error' => $error,
-          ]); 
-         }
-        $message = new Message();
-        $first_name = $data["first_name"] ?? '';
-        $last_name = $data["last_name"] ?? '';
-        $email = $data["email"] ?? '';
-        $phone = $data["phone"] ?? '';
-        $subject = $data["subject"] ?? '';
-        $content = $data["content"] ?? '';
-        $message->setFirstName($first_name);
-        $message->setLastName($last_name);
-        $message->setEmail($email);
-        $message->setPhone($phone);
-        $message->setSubject($subject);
-        $message->setContent($content);
-        $message->setCreatedAt(new DateTimeImmutable());
-        $emi->persist($message);
-        $emi->flush();
+
+            $message = new Message();
+            $first_name = $data["first_name"] ?? '';
+            $last_name = $data["last_name"] ?? '';
+            $email = $data["email"] ?? '';
+            $phone = $data["phone"] ?? '';
+            $subject = $data["subject"] ?? '';
+            $content = $data["content"] ?? '';
+  
+        if (
+            empty($first_name) ||
+            empty($last_name) ||
+            empty($email) ||
+            empty($phone) ||
+            empty($subject) ||
+            empty($content)
+        ) {
+            $error = "Veuillez renseigner tout les champs";
+        } else {
+
+            $message->setFirstName($first_name);
+            $message->setLastName($last_name);
+            $message->setEmail($email);
+            $message->setPhone($phone);
+            $message->setSubject($subject);
+            $message->setContent($content);
+            $message->setCreatedAt(new DateTimeImmutable());
+            $emi->persist($message);
+            $emi->flush();
+        }
         return $this->render('message/index.html.twig', [
             'first_name' => $first_name,
+            'error' => $error,
         ]);
     }
 }
