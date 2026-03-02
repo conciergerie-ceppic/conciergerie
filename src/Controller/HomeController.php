73eb792a4218910af4f50596bfaf98080f46cba3
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-03-02 16:46:48 +0100

    Ajout de la deuxieme map

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -34,6 +34,27 @@ final class HomeController extends AbstractController
 
     /// Autres méthodes
 
+    #[Route('/uploadAvatar', name: 'upload_avatar')]
+    public function uploadAvatar(Request $request, UserRepository $userRepository): Response
+    {
+        $avatar = $request->files->get('avatar');
+        // dd($avatar);
+        $directory = $this->getParameter('avatars_directory').'/';
+        // dd($directory);
+        $error = [];
+        $originalName = $avatar->getClientOriginalName();
+        $originalName = explode('.', $originalName);
+        $pathName = $avatar->getPathname();
+        // dd($originalName);
+        // dd($pathName);
+        $image = new \Gumlet\ImageResize($pathName);
+        $image->resizeToWidth(48);
+        $image->save($directory .$originalName[0].".webp", IMAGETYPE_WEBP);
+
+        // supprimer l'image ou l'uploader dans un catalogue si assez de temps
+        $userRepository->setAvatar($this->getUser()->getId() ,$originalName[0].".webp"); 
+        return $this->redirectToRoute('app_home');
+    }
     // Récupère la liste des services dans Enum
     public function listServices()
     {
