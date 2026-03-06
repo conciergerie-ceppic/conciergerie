2a7464cfdd22d8b64ef70951e68179e8e286a793
Kevin Calixte <kevincalixte@MacBook-Pro-de-Kevin.local>
2026-03-06 14:20:40 +0100

    Fix demande de position

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,7 +20,7 @@ final class HomeController extends AbstractController
         $lon = $session->get('lon');
 
         $ville = null;
-        if ($lat && $lon) {
+        if (!$lat && $lon) {
             $ville = $this->getVille($hci, (float)$lat, (float)$lon);
             $session->set('ville', $ville);
         }
